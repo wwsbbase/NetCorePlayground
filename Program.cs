c238eb54d1c02e9723144ac4888a67574e9ab88a
D.J. Zou <bopy2008@163.com>
2018-05-30 23:12:20 +0800

    移除多余文件

--- Program.cs
+++ Program.cs
@@ -4,8 +4,15 @@ namespace NetCorePlayground
 {
     class Program
     {
+        static void Time()
+        {
+            System.DateTime currentTime=new System.DateTime();
+            currentTime=System.DateTime.Now; 
+             Console.WriteLine(currentTime);
+        }
         static void Main(string[] args)
         {
+            Time();
             Console.WriteLine("Hello World!");
         }
     }
